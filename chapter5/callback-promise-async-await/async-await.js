fe83dda6b6f20638d706b3b0eab88b921439a8a3
HerDonghyuk <idnosukja@naver.com>
2023-06-07 16:17:28 +0900

    230607

--- chapter5/callback-promise-async-await/async-await.js
+++ chapter5/callback-promise-async-await/async-await.js
@@ -1,4 +1,4 @@
-async function myName() {
+/* async function myName() {
   return "Andy";
 }
 
@@ -19,6 +19,36 @@ function waitOneSecond(msg) {
 async function countOneToTen() {
   for (let x of [...Array(10).keys()]) {
     let result = await waitOneSecond(`${x + 1}초 대기중...`);
+    console.log(result);
+  }
+  console.log("완료");
+}
+
+countOneToTen();
+ */
+
+/* async function myName() {
+  return "Andy";
+}
+
+async function showName() {
+  const name = await myName();
+  console.log("1: " + name);
+}
+
+console.log("2: " + myName());
+console.log("3: " + showName());
+ */
+
+function waitOneSecond(msg) {
+  return new Promise((resolve, _) => {
+    setTimeout(() => resolve(`${msg}`), 1000);
+  });
+}
+
+async function countOneToTen() {
+  for (let x of [...Array(10).keys()]) {
+    let result = await waitOneSecond(`${x + 1}초 대기 중...`);
     console.log(result);
   }
   console.log("완료");
