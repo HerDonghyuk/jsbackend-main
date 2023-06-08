112f0b8e2d661018385122dc964902fc63ecbcc0
Her Donghyuk <idnosukja@naver.com>
2023-06-08 14:28:58 +0900

    230608

--- chapter6/try-mongo/test-connection.js
+++ chapter6/try-mongo/test-connection.js
@@ -1,10 +1,11 @@
 const { MongoClient } = require("mongodb");
-const uri = "mongodb+srv://mymongo:test1234@cluster0.c4xru.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri =
+  "mongodb+srv://idnosukja:cpdlswl1*@cluster0.c4xru.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri);
 
 async function run() {
   await client.connect();
-  const adminDB = client.db('test').admin();
+  const adminDB = client.db("test").admin();
   const listDatabases = await adminDB.listDatabases();
   console.log(listDatabases);
   return "OK";
