7365055c7aca7d798a4a5618aea3e47407f391af
Her Donghyuk <idnosukja@naver.com>
2023-06-13 23:18:34 +0900

    230613

--- chapter7/board/tempCodeRunnerFile.js
+++ chapter7/board/tempCodeRunnerFile.js
@@ -178,16 +178,18 @@ app.listen(3000, async () => {
 
 const express = require("express");
 const handlebars = require("express-handlebars");
-const app = express();
 const mongodbConnection = require("./configs/mongodb-connection");
+const postService = require("./services/post-service");
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.engine(
   "handlebars",
-  handlebars
-    .create({
-      helpers: require("./configs/handlebars-helpers"),
-    })
-    .engine,
+  handlebars.create({
+    helpers: require("./configs/handlebars-helpers"),
+  }).engine
 );
 app.set("view engine", "handlebars");
 app.set("views", __dirname + "/views");
@@ -198,6 +200,12 @@ app.get("/", (req, res) => {
 
 app.get("/write", (req, res) => {
   res.render("write", { title: "텍스트 게시판" });
+});
+
+app.post("/write", async (req, res) => {
+  const post = req.body;
+  const result = await postService.writePost(collection, post);
+  res.redirect(`/detail/${result.insertedId}`);
 });
 
 app.get("/detail/:id", async (req, res) => {
