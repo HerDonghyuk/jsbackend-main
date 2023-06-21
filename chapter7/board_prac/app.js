c4cef8def934ab329c3a41db4b99d033207aa018
HerDonghyuk <idnosukja@naver.com>
2023-06-21 15:49:51 +0900

    230621

--- chapter7/board_prac/app.js
+++ chapter7/board_prac/app.js
@@ -1,25 +1,13 @@
-/* const express = require("express");
-const handlebars = require("express-handlebars");
-const app = express();
-
-app.engine("handlebars", handlebars.engine());
-app.set("view engine", "handlebars");
-app.set("views", __dirname + "/views");
-
-app.get("/", (req, res) => {
-  res.render("home", { title: "안녕하세요", message: "반갑습니다."})
-}) */
-
 const express = require("express");
 const handlebars = require("express-handlebars");
 const app = express();
 
 app.engine("handlebars", handlebars.engine());
-app.set("view engine", "handlebars");
+app.set("view-engine", "handlebars");
 app.set("views", __dirname + "/views");
 
 app.get("/", (req, res) => {
-  res.render("home", { title: "안녕하세요?", message: "반급습니다" });
+  res.render("home", { title: "Hi,", message: "Hello!" });
 });
 
 app.listen(3000);
