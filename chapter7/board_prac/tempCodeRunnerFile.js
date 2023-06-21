70bba9f21a5bbfb1758303134646c003b6de1869
Her Donghyuk <idnosukja@naver.com>
2023-06-21 19:35:45 +0900

    230621

--- chapter7/board_prac/tempCodeRunnerFile.js
+++ chapter7/board_prac/tempCodeRunnerFile.js
@@ -7,15 +7,7 @@ app.set("view engine", "handlebars");
 app.set("views", __dirname + "/views");
 
 app.get("/", (req, res) => {
-<<<<<<< HEAD
   res.render("home", { title: "Hi,", message: "Hello!" });
-=======
-  res.render("home", { title: "테스트 게시판,", message: "Hello!" });
-});
-
-app.get("/write", (req, res) => {
-  res.render("write", { title: "테스트" });
->>>>>>> 4d6177dde10946b27f835ff96e867221a66cc8f8
 });
 
 app.listen(3000);
