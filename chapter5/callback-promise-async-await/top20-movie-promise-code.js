4a75b3c23d98ec859d03209fb6a3d2cb4758915a
Her Donghyuk <idnosukja@naver.com>
2023-07-11 23:32:36 +0900

    230711

--- chapter5/callback-promise-async-await/top20-movie-promise-code.js
+++ chapter5/callback-promise-async-await/top20-movie-promise-code.js
@@ -1,40 +1,20 @@
+// "https://raw.githubusercontent.com/wapj/musthavenodejs/main/movieinfo.json";
+
 const axios = require("axios");
 const url =
   "https://raw.githubusercontent.com/wapj/musthavenodejs/main/movieinfo.json";
 
-axios
-  .get(url)
-  .then((result) => {
-    if (result.status != 200) {
-      throw new Error("요청에 실패했습니다!");
-    }
+axios.get(url).then((result) => {
+  if (result.status != 200) {
+    throw new Error("request failure!");
+  }
 
-    if (result.data) {
-      return result.data;
-    }
+  if (result.data) {
+    return result.data;
+  }
 
-    throw new Error("데이터가 없습니다.");
-  })
-  .then((data) => {
-    if (!data.articleList || data.articleList.size == 0) {
-      throw new Error("데이터가 없습니다.");
-    }
-    return data.articleList;
-  })
-  .then((articles) => {
-    return articles.map((article, idx) => {
-      return { title: article.title, rank: idx + 1 };
-    });
-  })
-  .then((results) => {
-    for (let movieInfo of results) {
-      console.log(`[${movieInfo.rank}위] ${movieInfo.title}`);
-    }
-  })
-  .catch((err) => {
-    console.log("<<에러 발생!!>>");
-    console.error(err);
-  });
+  throw new Error("No Data");
+});
 
 /* const axios = require("axios");
 const url = "https://raw.githubusercontent.com/wapj/musthavenodejs/main/movieinfo.json";
