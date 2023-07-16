22b30bde77603b729fc174eaeffa8abf417c9ada
Her Donghyuk <idnosukja@naver.com>
2023-07-17 07:49:39 +0900

    230717

--- chapter7/board_prac/app.js
+++ chapter7/board_prac/app.js
@@ -1,47 +1,27 @@
-const express = require("express");
+/* const express = require("express");
 const handlebars = require("express-handlebars");
 const app = express();
-const mongodbConnection = require("./configs/mongodb-connection");
-/* const postService = require("./services/post-service"); */
-const postService = require("./services/post-service");
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.engine("handlebars", handlebars.engine());
+app.set("view engine", "handlebars");
+app.set("views", __dirname + "/vies");
 
-app.engine(
-  "handlebars",
-  handlebars.create({
-    helpers: require("./configs/handlebars-helpers"),
-  }).engine
-);
+app.get("/", (req, res) => {
+  res.render("home", { title: "Hi", message: "Nice to meet you"})
+});
 
+app.listen(3000); */
+
+const express = require("express");
+const app = express();
+const handlebars = require("express-handlebars");
+
+app.engine("handlebars", handlebars.engine());
 app.set("view engine", "handlebars");
 app.set("views", __dirname + "/views");
 
 app.get("/", (req, res) => {
-  res.render("home", { title: "테스트 게시판,", message: "Hello!" });
+  res.render("home", { title: "HI", message: "Hello" });
 });
 
-app.get("/write", (req, res) => {
-  res.render("write", { title: "테스트" });
-});
-
-/* app.post("/write", async (req, res) => {
-  const post = req.body;
-  const result = await postService.writePost(collection, post);
-  res.redirect(`detail/${result.insertedId}`);
-});  */
-
-app.post("/write", async (req, res) => {
-  const post = req.body;
-  const result = await postService.writePost(collection, post);
-  res.redirect(`detail/${result.insertedId}`);
-});
-
-let collection;
-app.listen(3000, async () => {
-  console.log("Server started");
-  const mongoClient = await mongodbConnection();
-  collection = mongoClient.db().collection("post");
-  console.log("MongoDB connected");
-});
+app.listen(3000);
