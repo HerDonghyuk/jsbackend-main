3c8e02ddf575832fba8022aad223217fcc2d5411
HerDonghyuk <idnosukja@naver.com>
2023-07-26 19:42:07 +0900

    230726

--- chapter7/board_prac/app.js
+++ chapter7/board_prac/app.js
@@ -16,6 +16,8 @@ const express = require("express");
 const handlebars = require("express-handlebars");
 const app = express();
 
+const mongodbConnection = require("./configs/mongodb-connection");
+
 app.engine("handlebars", handlebars.engine());
 app.set("view engine", "handlebars");
 app.set("views", __dirname + "/views");
@@ -32,4 +34,11 @@ app.get("/detail/:id", (req, res) => {
   res.render("detail", { title: "테스트 게시판" });
 });
 
-app.listen(3000);
+let collection;
+
+app.listen(3000, async () => {
+  console.log("Server started");
+  const mongoClient = await mongodbConnection();
+  collection = mongoClient.db().collection("post");
+  console.log("MongoDB connected");
+});
