462ac3ba5f0269a4f700c99859c23b799e453d54
Her Donghyuk <idnosukja@naver.com>
2023-07-31 23:33:52 +0900

    230731

--- chapter7/board_prac/configs/handlebars-helpers.js
+++ chapter7/board_prac/configs/handlebars-helpers.js
@@ -1,12 +1,5 @@
-/* module.export = {
-  lengthOfList: (list = []) => list.length,
-  eq: (val1, val2) => val1 === val2,
-  dateString: (isoString) => new Date(isoString).toLocaleDateString,
-};
- */
-
 module.exports = {
   lengthOfLists: (list = []) => list.length,
-  eq: (val1, val2) => val1 === val2,
+  eq: (val, val2) => val1 === val2,
   dateString: (isoString) => new Date(isoString).toLocaleDateString(),
 };
--- chapter7/board_prac/tempCodeRunnerFile.js
+++ chapter7/board_prac/tempCodeRunnerFile.js
@@ -16,7 +16,16 @@ const express = require("express");
 const handlebars = require("express-handlebars");
 const app = express();
 
-app.engine("handlebars", handlebars.engine());
+const mongodbConnection = require("./configs/mongodb-connection");
+
+app.engine(
+  "handlebars",
+  handlebars
+    .create({
+      helpers: require("./configs/handlebars-helpers"),
+    })
+    .engine
+);
 app.set("view engine", "handlebars");
 app.set("views", __dirname + "/views");
 
@@ -28,8 +37,15 @@ app.get("/write", (req, res) => {
   res.render("write", { title: "테스트 게시판" });
 });
 
-app.get("/detail", (req, res) => {
+app.get("/detail/:id", (req, res) => {
   res.render("detail", { title: "테스트 게시판" });
 });
 
-app.listen(3000);
+let collection;
+
+app.listen(3000, async () => {
+  console.log("Server Started");
+  const mongoClient = await mongodbConnection();
+  collection = mongoClient.db().collection("post");
+  console.log("MongoDB connected");
+});
