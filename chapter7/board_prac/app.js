6cfe4ff99e3253320ac550da1a26a1dea5a5c9ce
Her Donghyuk <idnosukja@naver.com>
2023-07-31 23:29:48 +0900

    230731

--- chapter7/board_prac/app.js
+++ chapter7/board_prac/app.js
@@ -16,9 +16,19 @@ const express = require("express");
 const handlebars = require("express-handlebars");
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const postService = require("./services/post-service");
+
 const mongodbConnection = require("./configs/mongodb-connection");
 
-app.engine("handlebars", handlebars.engine());
+app.engine(
+  "handlebars",
+  handlebars.create({
+    helpers: require("./configs/handlebars-helpers"),
+  }).engine
+);
 app.set("view engine", "handlebars");
 app.set("views", __dirname + "/views");
 
@@ -26,8 +36,10 @@ app.get("/", (req, res) => {
   res.render("home", { title: "테스트 게시판", message: "Nice to meet ya" });
 });
 
-app.get("/write", (req, res) => {
-  res.render("write", { title: "테스트 게시판" });
+app.post("/write", async (req, res) => {
+  const post = req.body;
+  const result = await postService.writePost(collection, post);
+  res.redirect(`/detail/${result.insertedId}`);
 });
 
 app.get("/detail/:id", (req, res) => {
@@ -35,9 +47,10 @@ app.get("/detail/:id", (req, res) => {
 });
 
 let collection;
+
 app.listen(3000, async () => {
-  console.log("Server started");
-  const mongodbClient = await mongodbConnection();
-  collection = mongodbClient.db().collection("post");
+  console.log("Server Started");
+  const mongoClient = await mongodbConnection();
+  collection = mongoClient.db().collection("post");
   console.log("MongoDB connected");
 });
