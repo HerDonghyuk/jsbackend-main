75bc4019c08ac72ae7ce3e84b1885f91c1a35146
Her Donghyuk <idnosukja@naver.com>
2023-08-17 14:20:28 +0900

    230817

--- chapter7/board/configs/mongodb-connection.js
+++ chapter7/board/configs/mongodb-connection.js
@@ -8,7 +8,8 @@ module.exports = function (callback) {
  */
 
 const { MongoClient } = require("mongodb");
-const uri = "mongodb+srv://idnosukja:ahdrhelql@cluster0.x9p1mqj.mongodb.net/test";
+const uri =
+  "mongodb+srv://idnosukja:ahdrhelql@cluster0.x9p1mqj.mongodb.net/test";
 
 module.exports = function (callback) {
   return MongoClient.connect(uri, callback);
