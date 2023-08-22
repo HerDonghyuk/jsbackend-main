47aac14a073f2aec628ce1c0ff9a71a9a47188fc
HerDonghyuk <idnosukja@naver.com>
2023-08-22 17:08:45 +0900

    230822

--- chapter7/board_prac/configs/handlebars-helpers.js
+++ chapter7/board_prac/configs/handlebars-helpers.js
@@ -1,6 +1,6 @@
 /* module.exports = {
   lengthOfLists: (list = []) => list.length,
-  eq: (val, val2) => val1 === val2,
+  eq: (val1, val2) => val1 === val2,
   dateString: (isoString) => new Date(isoString).toLocaleDateString(),
 };
  */
